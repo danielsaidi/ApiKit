c7408ff5f1434b29f4f5bcad9630840ffa659a49
Daniel Saidi <daniel.saidi@gmail.com>
2023-03-28 11:36:48 +0200

    Add demo app

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Daniel Saidi on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
