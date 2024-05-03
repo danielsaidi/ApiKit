1e47dc8a4f69f9255f64faef049bbf9ed4568bc3
Daniel Saidi <daniel.saidi@gmail.com>
2024-05-03 12:00:13 +0200

    Enable strict concurrency checking

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
     targets: [
         .target(
             name: "ApiKit",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
         ),
         .testTarget(
             name: "ApiKitTests",
